841623745c3039f74beed0ec150adc3a5cade046
jer <jer@moo>
2021-09-02 07:35:29 +0000

    add static site

---
+++ src/lib/component/IndexHero/index.svelte
@@ -0,0 +1,81 @@
+<script type="text/javascript">
+	import penabur from './logo-penabur.png';
+	import bee from './tightboi.png';
+	import stripedCircle from './striped-circle.svg';
+	import squiggle from './squiggle.svg';
+</script>
+
+<div class="relative">
+	<div class="absolute top-6 left-6 flex">
+		<img class="h-32" src={penabur} alt="logo-penabur" />
+		<img class="h-32" src={bee} alt="logo-bcomp" />
+	</div>
+	<img class="absolute top-0 right-0 w-56 z-0" src={stripedCircle} alt="" />
+	<img class="absolute top-64 left-0 w-24 z-0" src={squiggle} alt="" />
+</div>
+<div class="pt-4 pb-0 w-full max-w-lg mx-auto">
+	<svg viewBox="-100 -15 200 75">
+		<text
+			x="0"
+			y="0"
+			class="text-2xl font-alfa italic tracking-wide fill-current text-lavender"
+			dominant-baseline="central"
+			text-anchor="middle"
+		>
+			BRILLIANT
+		</text>
+		<text
+			x="1"
+			y="0.5"
+			class="text-2xl font-alfa italic tracking-wide fill-current text-lavender opacity-40"
+			dominant-baseline="central"
+			text-anchor="middle"
+		>
+			BRILLIANT
+		</text>
+
+		<text
+			x="4"
+			y="2"
+			class="text-2xl font-alfa italic tracking-wide stroke-current text-lavender stroke-1"
+			dominant-baseline="central"
+			fill="transparent"
+			text-anchor="middle"
+		>
+			BRILLIANT
+		</text>
+
+		<text
+			x="0"
+			y="25"
+			class="text-2xl font-barlow-semi italic tracking-wide fill-current text-coral"
+			dominant-baseline="central"
+			text-anchor="middle"
+		>
+			COMPETITION
+		</text>
+		<text
+			x="0"
+			y="28"
+			class="font-barlow-semi italic tracking-wide stroke-current text-coral"
+			dominant-baseline="central"
+			fill="transparent"
+			text-anchor="middle"
+			font-size="1.45rem"
+			stroke-width="0.5"
+		>
+			COMPETITION
+		</text>
+
+		<text
+			x="0"
+			y="50.5"
+			class="font-montserrat tracking-wider fill-current text-gray-700"
+			dominant-baseline="central"
+			text-anchor="middle"
+			font-size="0.625rem"
+		>
+			UNLOCKING YOUR BRILLIANCE
+		</text>
+	</svg>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,64 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import IndexHero from '$lib/component/IndexHero/index.svelte';
+	import school from './kalam-kudus.jpg';
+	import tightboi from './tightboi.png';
+</script>
+
+<svelte:head>
+	<title>Announcement · Brilliant Competition</title>
+</svelte:head>
+
+<div class="flex flex-col h-full">
+	<IndexHero />
+	<div class="mt-8 flex-grow">
+		<div class="container mx-auto">
+			<div class="max-w-7xl px-4 sm:px-6 lg:px-8 mx-auto">
+				<div class="flex space-x-16">
+					<div class="bg-white rounded-lg shadow-lg p-4">
+						<img
+							class="h-[20rem] min-h-[20rem] w-[20rem] min-w-[20rem] object-contain"
+							src={school}
+							alt="school logo"
+						/>
+					</div>
+
+					<div class="flex flex-col">
+						<div class="text-gray-700 text-5xl font-montserrat break-words">
+							Otto Alexander Sutianto
+						</div>
+						<div class="mt-8 text-gray-700 text-5xl font-montserrat break-words">
+							William Leonard Summendap
+						</div>
+						<div class="text-gray-500 text-3xl italic font-montserrat break-words">MAT-069</div>
+						<div class="flex-grow" />
+						<div class="text-gray-700 text-4xl font-montserrat break-words">
+							SMP Kalam Kudus Solo
+						</div>
+					</div>
+				</div>
+				<div class="mt-8 max-w-2xl mx-auto bg-coral-light rounded shadow p-4">
+					<ul class="font-barlow-semi text-gray-700 text-xl space-y-1">
+						<li>
+							MAT-001 &nbsp;&nbsp;|&nbsp;&nbsp; Otto Alexander Sutianto - William Leonard Sumendap
+						</li>
+						<li>
+							MAT-001 &nbsp;&nbsp;|&nbsp;&nbsp; Otto Alexander Sutianto - William Leonard Sumendap
+						</li>
+						<li>
+							MAT-001 &nbsp;&nbsp;|&nbsp;&nbsp; Otto Alexander Sutianto - William Leonard Sumendap
+						</li>
+						<li>
+							MAT-001 &nbsp;&nbsp;|&nbsp;&nbsp; Otto Alexander Sutianto - William Leonard Sumendap
+						</li>
+						<li>
+							MAT-001 &nbsp;&nbsp;|&nbsp;&nbsp; Otto Alexander Sutianto - William Leonard Sumendap
+						</li>
+						<li>
+							MAT-001 &nbsp;&nbsp;|&nbsp;&nbsp; Otto Alexander Sutianto - William Leonard Sumendap
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
