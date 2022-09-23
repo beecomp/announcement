9ff2c6fe7f20030252136ad5b43899fdb8a0a90c
Nicholas Ryan PH <nicholasryanph@gmail.com>
2022-09-23 09:52:58 +0700

    change smth

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,7 @@
   import cahayarancamaya from "./logo-cahayarancamaya.jpg";
   import darmayudha from "./logo-darmayudha.jpg";
   import globalprestasi from "./logo-globalprestasi.png";
+  import tarki3 from "./logo-tarki.jpg";
   import ipeka from "./logo-ipeka.png";
   import kalamkudus from "./logo-kalamkudus.jpg";
   import ketapang from "./logo-ketapang.png";
@@ -22,186 +23,40 @@
 	import bee from "./skinnyboiv1.png";
 
 	const participants = [
-    { // 0
+    {
       code: "S001",
       names: ["Baruna Adi Sanjaya, Narendra Aditya Tanoto"],
       school: "SMP Cahaya Rancamaya",
-      insignia: cahayarancamaya,
+      insignia: tarki3,
     },
-    // { // 1
-    //   code: "S003",
-    //   names: ["Satya Virya Atmadja"],
-    //   school: "SMPK Penabur Serang",
-    //   insignia: penabur,
-    // },
-    // { // 2
-    //   code: "S004",
-    //   names: ["Felicia Putri Gondokusumo, Herbert Karsten Juwono"],
-    //   school: "SMP Kristen Petra 3 Surabaya",
-    //   insignia: petra,
-    // },
-    // { // 3
-    //   code: "S005",
-    //   names: ["Fariz Rayyan Firdaus, Dzakwan Alvaro Putra"],
-    //   school: "SMP Kharisma Bangsa",
-    //   insignia: kharismabangsa,
-    // },
-    // { // 4
-    //   code: "S006",
-    //   names: ["Abdullah Karim, Pradipto Pandu Maheswara"],
-    //   school: "SMP Kharisma Bangsa",
-    //   insignia: kharismabangsa,
-    // },
-    // { // 5
-    //   code: "S009",
-    //   names: ["Richard Huang"],
-    //   school: "SMPK Penabur Gading Serpong",
-    //   insignia: penabur,
-    // },
-    // { // 6
-    //   code: "S010",
-    //   names: ["Justin, Edward Eugene Surya"],
-    //   school: "SMPK 2 Penabur",
-    //   insignia: penabur,
-    // },
-    { // 7
+
+    { 
       code: "S011",
       names: ["Stevenson Christopher Hudiono, Cheerish Natalia Rifel"],
       school: "SMPK 2 Penabur",
       insignia: penabur,
     },
-    // { // 8
-    //   code: "S012",
-    //   names: ["Adriel Nathanael Winfryd, Krislyn W Riyadi"],
-    //   school: "SMPK 7 Penabur",
-    //   insignia: penabur,
-    // },
-    // { // 9
-    //   code: "S013",
-    //   names: ["Cynthia Nikita Wibowo, Jeremee Rafael Wynn"],
-    //   school: "SMPK Penabur Kota Modern",
-    //   insignia: penabur,
-    // },
-    // { // 10
-    //   code: "S014",
-    //   names: ["Welfrydus Winnersdy Rante, Tiara Siddhi Fonso"],
-    //   school: "SMP Katolik Rajawali Makassar",
-    //   insignia: rajawali,
-    // },
-    // { // 11
-    //   code: "S015",
-    //   names: ["Justin Benaya Tirtadjaja, Fellyta Aquila Chiandra"],
-    //   school: "SMP Marsudirini Bogor",
-    //   insignia: marsudirini,
-    // },
-    // { // 12
-    //   code: "S016",
-    //   names: ["Kenneth Nicander Wijaya, Stefani Angela"],
-    //   school: "SMPK 6 Penabur",
-    //   insignia: penabur,
-    // },
-    // { // 13
-    //   code: "S017",
-    //   names: ["Vanessa Natalie Karlina, Katharine Lauren Tie"],
-    //   school: "SMP Santo Aloysius 2",
-    //   insignia: santoaloysius,
-    // },
-    // { // 14
-    //   code: "S018",
-    //   names: ["Abigail Venus Rashando, Calysta Bernice Prasasti"],
-    //   school: "SMP Santo Aloysius 2",
-    //   insignia: santoaloysius,
-    // },
-    // { // 15
-    //   code: "S019",
-    //   names: ["Joshua Emmanuel Wahyu, Vania Feby Wijaya"],
-    //   school: "SMPK 2 PENABUR",
-    //   insignia: penabur,
-    // },
-    { // 16
+
+    {
       code: "S020",
       names: ["Felix Halley Thamin, Gratia Plena Kusdevina"],
       school: "SMPK 4 Penabur",
       insignia: penabur,
     },
-    // { // 17
-    //   code: "S022",
-    //   names: ["Abraham Brandon Purnama"],
-    //   school: "SMP Kristen Ketapang 1 Jakarta",
-    //   insignia: ketapang,
-    // },
-    // { // 18
-    //   code: "S023",
-    //   names: ["Ethan Samuel Benaya Khu"],
-    //   school: "SMPK Penabur Kota Modern",
-    //   insignia: penabur,
-    // },
-    // { // 19
-    //   code: "S025",
-    //   names: ["Sammuel Lim, Anthony Christian"],
-    //   school: "SMP Kristen IPEKA Puri",
-    //   insignia: ipeka,
-    // },
-    { // 20
+
+    { 
       code: "S027",
       names: ["Jack Howard Wijaya, Juan Howard Wijaya"],
       school: "SMP Darma Yudha Pekanbaru",
       insignia: darmayudha,
     },
-    // { // 21
-    //   code: "S028",
-    //   names: ["Bagasmora Andreo Sibarani, Mikhael Faith Benaiah L. Saragih"],
-    //   school: "SMP Darma Yudha Pekanbaru",
-    //   insignia: darmayudha,
-    // },
-    // { // 22
-    //   code: "S029",
-    //   names: ["Khansky Khonery, Melvin Raphael Gandaatmaja"],
-    //   school: "Santa Laurensia Junior High School",
-    //   insignia: santalaurensia,
-    // },
-    { // 23
+    
+    {
       code: "S030",
       names: ["Nathan Raphael Martua N., William Tengganu"],
       school: "SMP Global Prestasi",
       insignia: globalprestasi,
     },
-    // { // 24
-    //   code: "S031",
-    //   names: ["Tsabitah Abiyyu, Joanna Stevia C."],
-    //   school: "SMP Global Prestasi",
-    //   insignia: globalprestasi,
-    // },
-    // { // 25
-    //   code: "S032",
-    //   names: ["Clara Christovia Sahetapy, Sevonarola Haryo Lumenta"],
-    //   school: "SMP Athalia",
-    //   insignia: athalia,
-    // },
-    // { // 26
-    //   code: "S033",
-    //   names: ["Feliska Adenia Lubitha, Cornelius Jabez Lim"],
-    //   school: "SMP Athalia",
-    //   insignia: athalia,
-    // },
-    // { // 27
-    //   code: "S034",
-    //   names: ["Matthew Christopher H, Kaitlyn Illian Toniman"],
-    //   school: "SMPK Penabur Gading Serpong",
-    //   insignia: penabur,
-    // },
-    // { // 28
-    //   code: "S035",
-    //   names: ["Gabriella Anna Santoso, Kayla Theodora Wibisana"],
-    //   school: "SMPK Kelapa Gading",
-    //   insignia: penabur,
-    // },
-    // { // 29
-    //   code: "S036",
-    //   names: ["Fiorine Audiera Han"],
-    //   school: "SMPK PENABUR Kota Modern",
-    //   insignia: penabur,
-    // },
   ];
 
 	const winners = [participants[2], participants[4], participants[0], participants[1], participants[3]];
