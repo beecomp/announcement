9fd89fec4500dfa96537b42da69435a717abea8e
Nicholas Ryan PH <nicholasryanph@gmail.com>
2022-09-23 12:25:46 +0700

    add logo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,58 +5,53 @@
 
 	import IndexHero from "$lib/component/IndexHero/index.svelte";
 
+  import malang from "./logo-1malang.png";
+  import jakarta4 from "./logo-4jakarta.png";
+  import jakarta41 from "./logo-41jakarta.png";
+  import alittihad from "./logo-allitihad.png";
   import athalia from "./logo-athalia.png";
-  import cahayarancamaya from "./logo-cahayarancamaya.jpg";
-  import darmayudha from "./logo-darmayudha.jpg";
-  import globalprestasi from "./logo-globalprestasi.png";
-  import tarki3 from "./logo-tarki.jpg";
+  import balikpapan from "./logo-balikpapan.png";
+  import banyuwangi from "./logo-banyumangi.png";
+  import bogorraya from "./logo-bogorraya.png";
+  import darmayudha from "./logo-darmayudha.png";
+  import depok from "./logo-depok.png";
+  import dianharapan from "./logo-dianharapan.png";
+  import insan from "./logo-insan.png";
+  import insanharapan from "./logo-insanharapan.png";
+  import insanparepare from "./logo-insanparepare.png";
   import ipeka from "./logo-ipeka.png";
-  import kalamkudus from "./logo-kalamkudus.jpg";
-  import ketapang from "./logo-ketapang.png";
+  import kalamkhudus from "./logo-kalamkhudus.png";
+  import katolikrajawali from "./logo-katolikrajawali.png";
+  import katoliksantaclara from "./logo-katoliksantaclara.png";
+  import kebayoran from "./logo-kebayoran.png";
   import kharismabangsa from "./logo-kharismabangsa.png";
-  import marsudirini from "./logo-marsudirini.jpg";
-  import penabur from "./logo-penabur.png";
-  import petra from "./logo-petra.png";
-  import rajawali from "./logo-rajawali.jpg";
-  import santalaurensia from "./logo-santalaurensia.png";
-  import santoaloysius from "./logo-santoaloysius.jpeg";
-	import bee from "./skinnyboiv1.png";
+  import kristenkenaan from "./logo-kristenkenaan.png";
+  import lamongan from "./logo-lamongan.png";
+  import maitreyawira from "./logo-maitreyawira.png";
+  import mentari from "./logo-mentari.png";
+  import mondial from "./logo-mondial.png";
+  import pahoa from "./logo-pahoa.png";  
+  import pangudiluhur from "./logo-pangudiluhur.png";
+  import penabur from "./logo-penabur.png";  
+  import pilarbangsa from "./logo-pilarbangsa.png";
+  import plbintang from "./logo-plbintang.png";  
+  import saintjohn from "./logo-saintjohn.png";
+  import stellamaris from "./logo-stellamaris.png";  
+  import strada from "./logo-strada.png";
+  import tarakanita from "./logo-tarakanita.png";  
+  import tarunabangsa from "./logo-tarunabangsa.png";
+  import tunasbangsa from "./logo-tunasbangsa.png";  
+  import witama from "./logo-witama.png";
+  import xaverius from "./logo-xaverius.png";
 
 	const participants = [
     {
       code: "S001",
       names: ["Baruna Adi Sanjaya, Narendra Aditya Tanoto"],
       school: "SMP Cahaya Rancamaya",
-      insignia: tarki3,
+      insignia: tarakanita,
     },
 
-    { 
-      code: "S011",
-      names: ["Stevenson Christopher Hudiono, Cheerish Natalia Rifel"],
-      school: "SMPK 2 Penabur",
-      insignia: penabur,
-    },
-
-    {
-      code: "S020",
-      names: ["Felix Halley Thamin, Gratia Plena Kusdevina"],
-      school: "SMPK 4 Penabur",
-      insignia: penabur,
-    },
-
-    { 
-      code: "S027",
-      names: ["Jack Howard Wijaya, Juan Howard Wijaya"],
-      school: "SMP Darma Yudha Pekanbaru",
-      insignia: darmayudha,
-    },
-    
-    {
-      code: "S030",
-      names: ["Nathan Raphael Martua N., William Tengganu"],
-      school: "SMP Global Prestasi",
-      insignia: globalprestasi,
-    },
   ];
 
 	const winners = [participants[2], participants[4], participants[0], participants[1], participants[3]];
