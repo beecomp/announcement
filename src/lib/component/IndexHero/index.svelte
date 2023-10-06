d351919ba60d763400925bd4ba258f670a1943c8
NicholasRyan <nicholasryanph@gmail.com>
2023-10-06 10:38:45 +0700

    add element, change design

--- src/lib/component/IndexHero/index.svelte
+++ src/lib/component/IndexHero/index.svelte
@@ -1,42 +1,42 @@
 <script type="text/javascript">
+	import a from './a.png';
+	import bnocorner from './bnocorner.png';
+	import c from './c.png';
 	import penabur from './logo-penabur.png';
-	import bee from './skinnyboiv1.png';
 	import hexA from './pat-hex-a.png';
-	import hexB from'./pat-hex-b.png';
-	import stripedPentagon from './striped-pentagon.svg';
-	import stripedCircle from './striped-circle.svg';
+	import hexB from './pat-hex-b.png';
+	import bee from './skinnyboiv1.png';
 	import squiggle from './squiggle.svg';
+	import stripedCircle from './striped-circle.svg';
+	import stripedPentagon from './striped-pentagon.svg';
 </script>
 
 <div class="relative">
 	<img class="absolute w-36 top-7 right-72 z-0" src={bee} alt="logo-bcomp" />
 	<img class="absolute top-10 left-72 w-36 z-0" src={penabur} alt="logo-penabur" />
-	<img class="absolute top-[40rem] left-0 w-64 z-0" src={hexA} alt="" />
-	<img class="absolute top-[40rem] right-0 w-64 z-0" src={hexB} alt="" />
-	<img class="absolute top-0 left-0 w-56 z-0" src={stripedPentagon} alt="" />
-	<img class="absolute top-0 right-0 w-56 z-0" src={stripedCircle} alt="" />
-	<img class="absolute top-64 left-0 w-24 z-0" src={squiggle} alt="" />
-	<svg class="absolute top-[32rem] right-0 w-48" viewBox="-100 0 105 50">
-		<rect class="fill-current text-yellow-500 opacity-70" x="-70" y="0" width="80" height="15" />
-		<rect class="fill-current text-yellow-500" x="-100" y="25" width="110" height="15" />
-	</svg>
+	<img class="absolute top-[40rem] left-10 w-64 z-0" src={c} alt="" />
+	<img class="absolute top-[40rem] right-0 w-64 z-0" src={bnocorner} alt="" />
+	<img class="absolute top-[40rem] left-32 w-56 z-0" src={a} alt="" />
+	<img class="absolute top-0 right-0 w-56 z-0" src={c} alt="" />
+	<img class="absolute top-64 left-0 w-24 z-0" src={hexA} alt="" />
 </div>
-<div class="pt-4 pb-0 w-full max-w-lg mx-auto">
+<div class="pt-4 pb-0 w-full max-w-lg mx-auto">		
 	<svg viewBox="-100 -15 200 75">
 		<text
 			x="4"
 			y="2"
-			class="text-2xl font-alfa italic tracking-wide stroke-current text-yellow-500 stroke-1"
+			class="text-2xl font-alfa italic tracking-wide text-yellow-300 fill-current"
 			dominant-baseline="central"
 			fill="transparent"
 			text-anchor="middle"
 		>
 			BRILLIANT
 		</text>
+
 		<text
 			x="0"
 			y="25"
-			class="text-2xl font-barlow-semi italic tracking-wide fill-current text-white"
+			class="text-2xl font-barlow-semi italic tracking-wide fill-current text-black"
 			dominant-baseline="central"
 			text-anchor="middle"
 		>
@@ -46,12 +46,12 @@
 		<text
 			x="0"
 			y="50.5"
-			class="font-montserrat tracking-wider fill-current text-white"
+			class="font-montserrat tracking-wider fill-current text-black"
 			dominant-baseline="central"
 			text-anchor="middle"
 			font-size="0.625rem"
 		>
-			A NEW AGE OF BRILLIANCE
+			PATH OF BRILLIANCE
 		</text>
 	</svg>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,11 @@
 <script>
-	import { fly, fade } from "svelte/transition";
-	import { onMount } from "svelte";
 	import _ from "lodash";
+	import { onMount } from "svelte";
+	import { fade, fly } from "svelte/transition";
 
 	import IndexHero from "$lib/component/IndexHero/index.svelte";
 	
+  import malang from "./logo-1malang.png";
   import athalia from "./logo-athalia.png";
   import bustanul from "./logo-bustanulmakmur.png";
   import cahayabangsa from "./logo-cahayabangsa.png";
@@ -18,15 +19,14 @@
   import mawar from "./logo-mawarsharon.png";
   import penabur from "./logo-penabur.png";
   import petra from "./logo-petra.png";
+  import bintanglaut from "./logo-plbintang.png";
   import pribadidepok from "./logo-pribadidepok.jpg";
   import raffles from "./logo-raffles.jpg";
   import rajawali from "./logo-rajawali.jpg";
-  import bintanglaut from "./logo-plbintang.png";
-  import skinnyboi from "./skinnyboiv1.png";
   import witama from "./logo-witama.jpg";
-  import malang from "./logo-1malang.png";
   import xaverius from "./logo-xaverius.jpg";
-	import bee from "./tightboi.png";
+  import skinnyboi from "./skinnyboiv1.png";
+  import bee from "./tightboi.png";
 	
   const participants = [
 
@@ -129,60 +129,62 @@
 
 <div class="flex flex-col h-full">
 	<IndexHero />
-	<div class="mt-8 flex-grow">
-		<div class="container mx-auto h-full">
-			<div class="flex flex-col h-full p-12 max-w-7xl px-4 sm:px-6 lg:px-8 mx-auto">
-				<div class="flex space-x-16">
-					{#key shown}
-						<div in:fly={{ y: 25, duration: 250 }} class="bg-white rounded-lg shadow-lg p-4 z-10">
-							<img
-								class="h-[20rem] min-h-[20rem] w-[20rem] min-w-[20rem] object-contain"
-								src={shown.insignia}
-								alt="school logo"
-							/>
-						</div>
-					{/key}
+		<div class="">
+			<div class="mt-8 flex-grow">
+				<div class="container mx-auto h-full">
+					<div class="flex flex-col h-full p-12 max-w-7xl px-4 sm:px-6 lg:px-8 mx-auto">
+						<div class="flex space-x-16">
+							{#key shown}
+								<div in:fly={{ y: 25, duration: 250 }} class="">
+									<img
+										class="h-[20rem] min-h-[20rem] w-[20rem] min-w-[20rem] object-contain"
+										src={shown.insignia}
+										alt="school logo"
+									/>
+								</div>
+							{/key}
 
-					<div class="flex flex-col">
-						{#key shown}
-							<div in:fly={{y: 50, duration: 250 }}>
-								<div class="text-white text-5xl font-montserrat break-words">
-									{shown.names[0]}
-								</div>
-								{#if shown.names[1]}
-									<div class="mt-8 text-white text-5xl font-montserrat break-words">
-										{shown.names[1]}
+							<div class="flex flex-col">
+								{#key shown}
+									<div in:fly={{ delay: 100, y: 50, duration: 250 }}>
+										<div class="text-black text-5xl font-montserrat break-words">
+											{shown.names[0]}
+										</div>
+										{#if shown.names[1]}
+											<div class="mt-8 text-black text-5xl font-montserrat break-words">
+												{shown.names[1]}
+											</div>
+										{/if}
+										<div class="text-black text-3xl italic font-montserrat break-words">
+											{shown.code}
+										</div>
 									</div>
-								{/if}
-								<div class="text-white text-3xl italic font-montserrat break-words">
-									{shown.code}
-								</div>
+								{/key}
+								<div class="flex-grow" />
+								{#key shown}
+									<div in:fly={{ y: 20, duration: 250 }}>
+										<div class="text-black text-4xl font-montserrat break-words">
+											{shown.school}
+										</div>
+									</div>
+								{/key}
 							</div>
-						{/key}
-						<div class="flex-grow" />
-						{#key shown}
-							<div in:fly={{ y: 20, duration: 250 }}>
-								<div class="text-white text-4xl font-montserrat break-words">
-									{shown.school}
-								</div>
-							</div>
-						{/key}
+						</div>
+						<div class="mt-8 w-full flex-grow max-w-xl mx-auto p-4">
+							<ul class="font-barlow-semi text-black text-l space-y-1">
+								{#each _.take(shownWinners, 6) as t}
+									<li class="text-center">
+										{t.code} &nbsp;&nbsp;|&nbsp;&nbsp; {t.names.join(" - ")}
+									</li>
+								{/each}
+							</ul>
+						</div>
+						<button
+							on:click={() => magic()}
+							class="block w-full mt-4 h-2 max-w-2xl mx-auto bg-lavender rounded shadow p-4"
+						/>
 					</div>
 				</div>
-				<div class="mt-8 w-full flex-grow max-w-xl mx-auto p-2">
-					<ul class="font-barlow-semi text-white text-l space-y-1">
-						{#each _.take(shownWinners, 6) as t}
-							<li class="text-center">
-								{t.code} &nbsp;&nbsp;|&nbsp;&nbsp; {t.names.join(" - ")}
-							</li>
-						{/each}
-					</ul>
-				</div>
-				<button
-					on:click={() => magic()}
-					class="block w-full mt-4 h-2 max-w-xl mx-auto bg-lavender rounded shadow p-4"
-				/>
 			</div>
 		</div>
-	</div>
 </div>
